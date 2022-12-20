5f9dec3d1ebaf766fdf4d0ba10fd363277a1d3a9
codecode <lutroner@ya.ru>
2022-12-21 01:39:37 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,65 @@
+import argparse
+import os
+
+import requests
+import telegram
+from dotenv import load_dotenv
+
+DVMN_URL = "https://dvmn.org/api/long_polling/"
+TIMEOUT = 92
+
+
+def get_response(url, dvmn_token, params=None):
+    headers = {"Authorization": f"Token {dvmn_token}"}
+    response = requests.get(url, headers=headers, timeout=TIMEOUT, params=params)
+    response.raise_for_status()
+    return response.json()
+
+
+def send_telegram_notification(response, bot_token, chat_id):
+    bot = telegram.Bot(token=bot_token)
+    lesson_title = response["new_attempts"][0]["lesson_title"]
+    lesson_url = response["new_attempts"][0]["lesson_url"]
+    if response["new_attempts"][0]["is_negative"]:
+        bot.send_message(
+            chat_id=chat_id,
+            text=f'У вас проверили работу "{lesson_title}" - {lesson_url} '
+            "К сожалению в работе нашлись ошибки.",
+        )
+    else:
+        bot.send_message(
+            chat_id=chat_id,
+            text=f'У вас проверили работу "{lesson_title}" - {lesson_url} '
+            "Преподавателю все понравилось, можно приступать к следующему уроку!",
+        )
+
+
+def main(chat_id):
+    load_dotenv()
+    bot_token = os.environ.get("BOT_TOKEN")
+    dvmn_token = os.environ.get("DEVMAN_TOKEN")
+    last_timestamp = None
+    while True:
+        params = {"timestamp": last_timestamp}
+        try:
+            response = get_response(DVMN_URL, dvmn_token, params)
+        except requests.exceptions.ReadTimeout:
+            continue
+        except requests.exceptions.ConnectionError:
+            continue
+        if "timestamp_to_request" in response:
+            last_timestamp = response["timestamp_to_request"]
+        else:
+            last_timestamp = response["last_attempt_timestamp"]
+            send_telegram_notification(response, bot_token, chat_id)
+
+
+if __name__ == "__main__":
+    notification_bot = argparse.ArgumentParser(
+        description="Бот для отправки уведомлений о проверке работ на dvmn.org"
+    )
+    notification_bot.add_argument(
+        "--chat_id", default=os.getenv("CHAT_ID"), help="chat id Telegram бота"
+    )
+    chat_id = notification_bot.parse_args().chat_id
+    main(chat_id)
